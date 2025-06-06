4afff2fac8304bef9562f11a56b5e3d6cdc9cecb
dplochcoder <daniel.ploch.coder@gmail.com>
2025-06-05 20:33:03 -0400

    Order item first

--- GrubDay2025Plando/AdvancedMimic.cs
+++ GrubDay2025Plando/AdvancedMimic.cs
@@ -12,6 +12,24 @@ using UnityEngine;
 
 namespace GrubDay2025Plando;
 
+internal class AdvancedMimic : MimicItem
+{
+    static AdvancedMimic() => Container.DefineContainer<AdvancedMimicContainer>();
+
+    public float? Scale;
+    public float? MaxSpeed;
+    public float? Acceleration;
+
+    public override string GetPreferredContainer() => AdvancedMimicContainer.AdvancedMimic;
+
+    public override bool GiveEarly(string containerType) => containerType == AdvancedMimicContainer.AdvancedMimic;
+
+    public override void GiveImmediate(GiveInfo info)
+    {
+        if (info.Container != AdvancedMimicContainer.AdvancedMimic) base.GiveImmediate(info);
+    }
+}
+
 internal class AdvancedMimicContainer : MimicContainer
 {
     internal const string AdvancedMimic = "AdvancedMimic";
@@ -90,23 +108,5 @@ internal class AdvancedMimicContainer : MimicContainer
                 Transform = mimic.transform
             });
         }
-    }
-}
-
-internal class AdvancedMimic : MimicItem
-{
-    static AdvancedMimic() => Container.DefineContainer<AdvancedMimicContainer>();
-
-    public float? Scale;
-    public float? MaxSpeed;
-    public float? Acceleration;
-
-    public override string GetPreferredContainer() => AdvancedMimicContainer.AdvancedMimic;
-
-    public override bool GiveEarly(string containerType) => containerType == AdvancedMimicContainer.AdvancedMimic;
-
-    public override void GiveImmediate(GiveInfo info)
-    {
-        if (info.Container != AdvancedMimicContainer.AdvancedMimic) base.GiveImmediate(info);
     }
 }
